ea273e123d36bb9b51e605f1c11c24aa7d9f4c37
cl <5712679+C8Luna@users.noreply.github.com>
2024-05-17 14:08:33 -0500

    anded a new merge func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+// impl Point {
+//     fn new(x: i32, y: i32) -> Point {
+//         Point { x: x, y: y }
+//     }
+//     fn distance(&self, other: &Point) -> f64 {
+//         let dx = self.x - other.x;
+//         let dy = self.y - other.y;
+//         ((dx * dx + dy * dy) as f64).sqrt()
+//     }
+//     fn x(&self) -> i32 {
+//         self.x
+//     }
+// }
